a7fd25ec599933410c1f63c5649e997c3e31a886
Richtoji <richtoji@gmail.com>
2026-05-07 10:19:11 +0530

    Fix missing HttpResponse import in emergency admin view

--- feedback/views.py
+++ feedback/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.db.models import Avg, Q
 from django.core.paginator import Paginator
 from django.contrib import messages
